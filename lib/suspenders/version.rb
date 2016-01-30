0b467831f3657df4ad9d63b0227af5a7787b1217
Nick Giancola <nick@gophilosophie.com>
2016-01-30 07:51:15 -0800

    Bump versions

--- lib/suspenders/version.rb
+++ lib/suspenders/version.rb
@@ -1,5 +1,5 @@
 module Suspenders
-  RAILS_VERSION = "4.2.5"
+  RAILS_VERSION = "4.2.5.1"
   RUBY_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip
-  VERSION = "2.0.2"
+  VERSION = "2.1.0"
 end
