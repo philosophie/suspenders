e4650f210ea24428c67145361a78026fd95da6d2
ethanj <ethanj@gmail.com>
2018-08-09 16:00:22 -0700

    Update version.rb

--- lib/suspenders/version.rb
+++ lib/suspenders/version.rb
@@ -1,5 +1,5 @@
 module Suspenders
-  RAILS_VERSION = "~> 5.1.4"
+  RAILS_VERSION = "~> 5.2.1"
   RUBY_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip
   VERSION = "2.2.2"
 end
