33226a761357dd6cefe0e02215cdf72b992a2297
Ashwin Vaswani <ashwinv11@gmail.com>
2019-05-17 10:09:08 -0700

    bump rails and ruby version (#120)

--- lib/suspenders/version.rb
+++ lib/suspenders/version.rb
@@ -1,5 +1,5 @@
 module Suspenders
-  RAILS_VERSION = "~> 5.2.1"
+  RAILS_VERSION = "~> 5.2.3"
   RUBY_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip
-  VERSION = "2.2.4"
+  VERSION = "2.2.5"
 end
